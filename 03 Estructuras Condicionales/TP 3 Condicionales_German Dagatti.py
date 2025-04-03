d6577b39f2d82d43e6434a15081c96140a70f08d
gdagatti <gdagatti@gmail.com>
2025-04-03 00:14:18 -0300

    Se corrige error en Ejercicio 10

--- 03 Estructuras Condicionales/TP 3 Condicionales_German Dagatti.py
+++ 03 Estructuras Condicionales/TP 3 Condicionales_German Dagatti.py
@@ -250,22 +250,22 @@ dia = int(input("Ingrese el día de hoy en números (1-31). Ej: Para lunes 10 so
 
 # Determinar la estación según el hemisferio y la fecha
 if hemisferio == "N":
-    if (mes == 12 and dia > 21) or (mes in [1, 2]) or (mes == 3 and dia <= 20):
+    if (mes == 12 and dia >= 21) or (mes in [1, 2]) or (mes == 3 and dia <= 20):
         estacion = "Invierno"
-    elif (mes == 3 and dia > 21) or (mes in [4, 5]) or (mes == 6 and dia <= 20):
+    elif (mes == 3 and dia >= 21) or (mes in [4, 5]) or (mes == 6 and dia <= 20):
         estacion = "Primavera"
-    elif (mes == 6 and dia > 21) or (mes in [7, 8]) or (mes == 9 and dia <= 20):
+    elif (mes == 6 and dia >= 21) or (mes in [7, 8]) or (mes == 9 and dia <= 20):
         estacion = "Verano"
-    elif (mes == 9 and dia > 21) or (mes in [10, 11]) or (mes == 12 and dia <= 20):
+    elif (mes == 9 and dia >= 21) or (mes in [10, 11]) or (mes == 12 and dia <= 20):
         estacion = "Otoño"
 elif hemisferio == "S":
-    if (mes == 12 and dia > 21) or (mes in [1, 2]) or (mes == 3 and dia <= 20):
+    if (mes == 12 and dia >= 21) or (mes in [1, 2]) or (mes == 3 and dia <= 20):
         estacion = "Verano"
-    elif (mes == 3 and dia > 21) or (mes in [4, 5]) or (mes == 6 and dia <= 20):
+    elif (mes == 3 and dia >= 21) or (mes in [4, 5]) or (mes == 6 and dia <= 20):
         estacion = "Otoño"
-    elif (mes == 6 and dia > 21) or (mes in [7, 8]) or (mes == 9 and dia <= 20):
+    elif (mes == 6 and dia >= 21) or (mes in [7, 8]) or (mes == 9 and dia <= 20):
         estacion = "Invierno"
-    elif (mes == 9 and dia > 21) or (mes in [10, 11]) or (mes == 12 and dia <= 20):
+    elif (mes == 9 and dia >= 21) or (mes in [10, 11]) or (mes == 12 and dia <= 20):
         estacion = "Primavera"
 else:
     estacion = "Hemisferio no válido"
